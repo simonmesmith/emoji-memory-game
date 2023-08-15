3389f13bf0a89cadd014cf9dc30ccc53c0f83945
Simon Smith <simon@benchsci.com>
2023-08-15 14:08:12 -0400

    Close #3 by showing total tries when user wins

--- src/components/GameStatus.js
+++ src/components/GameStatus.js
@@ -1,10 +1,9 @@
 const GameStatus = ({ isGameWon, turns }) => (
     <div className="text-center mb-4">
-        <div className={`${isGameWon ? 'text-green-500 italic' : 'text-blue-600'} text-xl`}>
-            {isGameWon ? "Congratulations! You've won!" : `Turns: ${turns}`}
+        <div className={`${isGameWon ? 'text-green-500' : 'text-blue-600'} text-xl`}>
+            {isGameWon ? `Congratulations! You've won in ${turns} turns!` : `Turns: ${turns}`}
         </div>
     </div>
-
 );
 
 export default GameStatus;
